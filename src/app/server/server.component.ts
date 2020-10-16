f179adebf275275cb0b2bdee8cee200206bb29e6
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2020-10-16 05:35:09 +0200

    updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,14 +4,31 @@ import { Server } from '../shared/server';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  color: string;
+  buttonText: string;
 
   @Input() serverInput: Server;
   ngOnInit(): void {
+    this.SetServerStatus(this.serverInput.IsOnline);
   }
 
+  SetServerStatus = (isOnline: boolean) => {
+    if (isOnline) {
+      this.serverInput.IsOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.IsOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  };
+  ToggleStatus = (onlineStatus: boolean) => {
+    this.SetServerStatus(!onlineStatus);
+  };
 }
