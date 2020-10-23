b8e66aa24c71acd118290526d2c3c139243d1322
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2020-10-23 20:21:56 +0200

    added server-message model

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ServerMessage } from '../shared/server-message';
+import { Server } from '../shared/server';
 
 @Injectable({
   providedIn: 'root'
